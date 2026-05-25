b73180d049e3ce812450cde5cf366b2448fb5228
Stepan Postranecky <stepan.postranecky@alteryx.com>
2026-05-25 11:15:44 +0200

    pages change via gemini

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,7 @@ import { defineUserConfig } from 'vuepress'
 import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
+  base: '/',
   lang: 'en-US',
 
   title: 'Recepty',
